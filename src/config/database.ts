17f6aec759a91941137e00a4156db7d5879b545d
Mariana Salmaza <mariana.salmaza@grupointegrado.br>
2025-03-16 22:19:01 -0300

    chore: criando conexão com o banco de dados usando Sequelize

---
+++ src/config/database.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const DB_USER = process.env.DB_USER || "user_undefined";
+const DB_USER_PASSWORD = process.env.DB_USER_PASSWORD;
+const DB_NAME = process.env.DB_NAME || "name_unedfined";
+const DB_HOST = process.env.DB_HOST;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_USER_PASSWORD, {
+  host: DB_HOST,
+  dialect: "mysql",
+});
+
+export default sequelize;
