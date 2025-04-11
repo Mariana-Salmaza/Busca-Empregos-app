9a40291e5131ec8da7dd5a03bb523f30dcbf9a5b
Mariana Salmaza <mariana.salmaza@grupointegrado.br>
2025-04-11 19:09:21 -0300

    refactor: apagando arquivos residuais

--- src/routes/LoginRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { loginUser } from "../controllers/LoginController";
-
-const router = express.Router();
-
-router.post("/", loginUser);
-
-export default router;
--- src/tests/simples.test.ts
+++
@@ -1,7 +0,0 @@
-function soma(a: number, b: number): number {
-  return a + b;
-}
-
-test("soma 2 + 2 deve ser 4", () => {
-  expect(soma(2, 2)).toBe(4);
-});
